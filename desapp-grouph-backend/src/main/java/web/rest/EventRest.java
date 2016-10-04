8518b7c49e84cfe1e1981004eef3ece64336c7e7
Matias <mz.matiazzz@gmail.com>
2016-10-04 13:47:48 -0300

    ejemplo funciona con jsons

--- desapp-grouph-backend/src/main/java/web/rest/EventRest.java
+++ desapp-grouph-backend/src/main/java/web/rest/EventRest.java
@@ -1,5 +1,11 @@
 package web.rest;
 
+import model.events.Event;
+import model.events.EventData;
+import model.events.FoodEvent;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,5 +18,16 @@ public class EventRest {
 	@Produces("text/plain")
 	public String example(){
 		return "Works!";
+	}
+
+	@GET
+	@Path("/example2")
+	@Produces("application/json")
+	public Event example2(){
+		FoodEvent foodEvent = new FoodEvent();
+		EventData ed = new EventData("Salida", "Con amigos", foodEvent, 100,
+				"Calle 25, La Plata", new LocalDate(), new LocalTime(), 2);
+		Event event = new Event(ed);
+		return event;
 	}
 }
