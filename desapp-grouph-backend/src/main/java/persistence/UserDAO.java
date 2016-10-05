e265d73f6cd6a6d6d4a647909c622d67afd0f6b1
Matias <mz.matiazzz@gmail.com>
2016-10-05 12:59:06 -0300

    agregado userDao

---
+++ desapp-grouph-backend/src/main/java/persistence/UserDAO.java
@@ -0,0 +1,11 @@
+package persistence;
+
+import model.users.User;
+
+public class UserDAO extends HibernateGenericDAO<User> implements GenericRepository<User> {
+
+    @Override
+    protected Class<User> getDomainClass() {
+        return User.class;
+    }
+}
