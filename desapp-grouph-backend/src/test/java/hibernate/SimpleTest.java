63350a493fc94a70aa3bf841b0262e8f84c9562d
martin <martin.ramos292@gmail.com>
2016-10-15 16:56:11 -0300

    Agrego test

--- desapp-grouph-backend/src/test/java/hibernate/SimpleTest.java
+++ desapp-grouph-backend/src/test/java/hibernate/SimpleTest.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import service.UserService;
+import static model.builders.UserBuilder.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/META-INF/spring-persistence-context.xml", "/META-INF/spring-services-context.xml" })
@@ -22,5 +24,15 @@ public class SimpleTest {
     public void saveUserTest(){
         userService.save(new User());
         assertEquals(1, userService.retriveAll().size());
+    }
+
+    public void shouldUpgradeUser(){
+        User user = anyUser().withUserName("NOMVRE").build();
+        userService.save(user);
+
+        user.setName("NOMBRE");
+        userService.update(user);
+
+        assertTrue(userService.retriveAll().contains(user));
     }
 }
