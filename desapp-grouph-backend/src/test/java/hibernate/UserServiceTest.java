804152d411c610db6a7d01da9e2cb03924a53d9d
martin <martin.ramos292@gmail.com>
2016-10-15 17:24:44 -0300

    Agrego UserServiceTest

--- desapp-grouph-backend/src/test/java/hibernate/UserServiceTest.java
+++ desapp-grouph-backend/src/test/java/hibernate/UserServiceTest.java
@@ -7,21 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import service.UserService;
-
+import static model.builders.UserBuilder.anyUser;
 import static org.junit.Assert.assertEquals;
+
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/META-INF/spring-persistence-context.xml", "/META-INF/spring-services-context.xml" })
-public class SimpleTest {
+public class UserServiceTest {
 
     @Autowired
     private UserService userService;
 
-
     @Test
-    public void saveUserTest(){
-        userService.save(new User());
-        assertEquals(1, userService.retriveAll().size());
+    public void shouldUpgradeUser(){
+        User user = anyUser().withUserName("NOMVRE").build();
+        userService.save(user);
+
+        user.setName("NOMBRE");
+        userService.update(user);
+
+        User usr = userService.retriveAll().get(0);
+        assertEquals(usr.getName(), "NOMBRE");
     }
 
 }
