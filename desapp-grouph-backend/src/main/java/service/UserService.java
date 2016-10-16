94bb9e3a3c258c6b64b2e36a6d11fbe9efec880b
Matias <mz.matiazzz@gmail.com>
2016-10-15 23:34:10 -0300

    actualizo test hibernate

--- desapp-grouph-backend/src/main/java/service/UserService.java
+++ desapp-grouph-backend/src/main/java/service/UserService.java
@@ -1,9 +1,28 @@
 package service;
 
 import model.users.User;
+import org.springframework.transaction.annotation.Transactional;
+import persistence.UserDAO;
+
+import java.util.List;
 
 public class UserService extends GenericService<User> {
 
 	private static final long serialVersionUID = -2932116622242535843L;
 
+	private UserDAO userDAO;
+
+	@Transactional
+	public List<User> findByName(String name) {
+		return this.userDAO.findByName(name);
+	}
+
+	@Transactional
+	public List<User> findByUserName(String userName) {
+		return this.userDAO.findByUserName(userName);
+	}
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
 }
--- desapp-grouph-backend/src/test/java/hibernate/SimpleTest.java
+++ desapp-grouph-backend/src/test/java/hibernate/SimpleTest.java
@@ -1,17 +1,19 @@
 package hibernate;
 
 import model.users.User;
-import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import service.UserService;
 import static model.builders.UserBuilder.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/META-INF/spring-persistence-context.xml", "/META-INF/spring-services-context.xml" })
 public class SimpleTest {
@@ -19,20 +21,19 @@ public class SimpleTest {
     @Autowired
     private UserService userService;
 
-
     @Test
-    public void saveUserTest(){
+    public void shouldSaveUserTest(){
         userService.save(new User());
         assertEquals(1, userService.retriveAll().size());
     }
 
+    @Test
     public void shouldUpgradeUser(){
-        User user = anyUser().withUserName("NOMVRE").build();
+        User user = anyUser().withUserName("anyName").build();
         userService.save(user);
-
-        user.setName("NOMBRE");
+        user.setName("aName");
         userService.update(user);
-
-        assertTrue(userService.retriveAll().contains(user));
+        assertEquals(0, userService.findByName("anyName").size());
+        assertEquals(1, userService.findByName("aName").size());
     }
 }
