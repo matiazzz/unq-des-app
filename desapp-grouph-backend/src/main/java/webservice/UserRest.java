8ebd44eda68d161dac0b374e1d51fbea4ad86ee3
martin <martin.ramos292@gmail.com>
2016-10-18 11:37:50 -0300

    Actualizo

--- desapp-grouph-backend/src/main/java/webservice/UserRest.java
+++ desapp-grouph-backend/src/main/java/webservice/UserRest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
 
 @Path("/user")
 public class UserRest {
@@ -17,22 +18,22 @@ public class UserRest {
     @GET
     @Path("/users/{name}")
     @Produces("application/json")
-    public List<User> findUsersByName(String name){
+    public List<User> findUsersByName(@PathParam("name") String name){
         return service.findByName(name);
     }
 
     @GET
     @Path("/user/{userName}")
     @Produces("application/json")
-    public User findUserByUserName(String userName) { return service.findByUserName(userName); }
+    public User findUserByUserName(@PathParam("userName") String userName) { return service.findByUserName(userName); }
 
     @GET
     @Path("/userProfile/{userName}")
     @Produces("application/json")
-    public Profile getProfileByUserName(String userName) {return service.getProfileByUserName(userName); }
+    public Profile getProfileByUserName(@PathParam("userName") String userName) {return service.getProfileByUserName(userName); }
 
     @GET
-    @Path("/user/{ID}")
+    @Path("/user/{id}")
     @Produces("application/json")
-    public User findUserByID(int id) {return service.findByID(id); }
+    public User findUserByID(@PathParam("id") int id) {return service.findByID(id); }
 }
