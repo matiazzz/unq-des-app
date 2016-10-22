a2348556f708e1363294e8f28dae45dbcb2abdff
Matias <mz.matiazzz@gmail.com>
2016-10-21 23:25:25 -0300

    add new frontend project

---
+++ desapp-grouph-frontend/README.md
@@ -0,0 +1,17 @@
+# desapp-grouph-frontend
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+`npm install`  
+`bower install`  
+`grunt serve`  
+  
+Run `grunt` for building and `grunt serve` for preview.
+
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ desapp-grouph-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGrouphFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the desappGrouphFrontendApp
+ */
+angular.module('desappGrouphFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
