87672b8dc756c2eb80963e378f49b1031d1086d8
Matias <mz.matiazzz@gmail.com>
2016-10-25 14:22:05 -0300

    actualizo events.js

--- desapp-grouph-frontend/app/scripts/controllers/events.js
+++ desapp-grouph-frontend/app/scripts/controllers/events.js
@@ -1,25 +1,6 @@
 'use strict';
 
-/*angular.module('desappGrouphFrontendApp')
-  .controller('EventsCtrl', function () {
+angular.module('desappGrouphFrontendApp')
+.controller('EventsCtrl', function () {
 
-  });*/
-
-  angular.module('desappGrouphFrontendApp');
-  function CarouselDemoCtrl($scope){
-    $scope.myInterval = 3000;
-    $scope.slides = [
-      {
-        image: 'http://lorempixel.com/400/200/'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/food'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/sports'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/people'
-      }
-    ];
-  }
+});
