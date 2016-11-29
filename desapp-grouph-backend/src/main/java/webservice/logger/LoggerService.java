43a7f8cc5e25f3532a877eb94dded837bab0bab2
martin <martin.ramos292@gmail.com>
2016-11-29 14:27:50 -0300

    Actualizo

--- desapp-grouph-backend/src/main/java/webservice/logger/LoggerService.java
+++ desapp-grouph-backend/src/main/java/webservice/logger/LoggerService.java
@@ -6,17 +6,19 @@ import org.apache.log4j.BasicConfigurator;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 
 //import java.util.Arrays;
 
 @Aspect
+@Order(1)
 public class LoggerService {
 
     public final static Logger log = Logger.getLogger(LoggerService.class);
 
-    @Before("execution(* service.*.*(..))")
-    public void before(){
-        log.info("Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    @Before("execution(* webservice.*.*(..))")
+    public void before(JoinPoint jp){
+        log.info("*****************************************************");
     }
 
     public LoggerService(){PropertyConfigurator.configure("src/main/resources/log4j.properties");}
