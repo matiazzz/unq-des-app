a45e2b991526f0bc6e7e5eb0ea78b13ff095df1d
martin <martin.ramos292@gmail.com>
2016-12-05 10:54:54 -0300

    Actualizo (falta aplicar filtros y paginacion)

--- desapp-grouph-frontend/app/scripts/controllers/events/proposals.js
+++ desapp-grouph-frontend/app/scripts/controllers/events/proposals.js
@@ -1,5 +1,39 @@
 'use strict';
 
-angular.module('desappGrouphFrontendApp').controller('ProposalsCtrl', function ($scope, eventServices) {
-	//
+angular.module('desappGrouphFrontendApp').controller('ProposalsCtrl', function ($scope, $http) {
+
+	$http.get('https://unq-des-app.herokuapp.com/rest/event/getAll').then(function(response) {
+        $scope.events = response.data;
+    });
+
+	function filterGetFreeEvents(){
+		$http.get('https://unq-des-app.herokuapp.com/rest/event/getFreeEvents').then(function(response) {
+        $scope.events = response.data;
+    	});
+	}
+
+	function filterGetWithFriendsEvents(){
+		$http.get('https://unq-des-app.herokuapp.com/rest/event/getWithFriendsEvents').then(function(response) {
+        $scope.events = response.data;
+    	});
+	}
+
+	function filterGetTodayEvents(){
+		$http.get('https://unq-des-app.herokuapp.com/rest/event/getTodayEvents').then(function(response) {
+        $scope.events = response.data;
+    	});
+	}
+
+	function filterGetWithCoupleEvents(){
+		$http.get('https://unq-des-app.herokuapp.com/rest/event/getWithCoupleEvents').then(function(response) {
+        $scope.events = response.data;
+    	});
+	}
+
+	function filterGetSuprisedMeEvents(){
+		$http.get('https://unq-des-app.herokuapp.com/rest/event/getSuprisedMeEvents').then(function(response) {
+        $scope.events = response.data;
+    	});
+	}
+
 });
